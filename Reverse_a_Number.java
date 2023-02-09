064b7eab7a5f0454c097e49df6f56c7669ec0103
rishab2512 <68326545+rishab2512@users.noreply.github.com>
2023-02-09 16:31:42 +0530

    Create Reverse_a_Number.java

---
+++ Reverse_a_Number.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class MyClass {
+    public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+    int number = sc.nextInt();
+    int reverse = 0;
+     while(number != 0){
+         int remainder = number%10;
+         reverse = reverse * 10 + remainder;
+         number = number / 10;
+     }
+     System.out.print("Reverse of number is: "+reverse);
+    }
+}
